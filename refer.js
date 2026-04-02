44ee2819b85409c00d58081b863fc4761bbee96b
digitaltrev <99693093+digitaltrev@users.noreply.github.com>
2026-04-02 17:39:38 -0600

    Update refer.js

--- refer.js
+++ refer.js
@@ -1,6 +1,6 @@
 document.getElementById('raf-share-btn').addEventListener('click', function(e) {
   e.preventDefault();
-  var msg = "Open an account at MyFI and we'll both earn $50! My referral code is [YOUR-CELL]. Click here: https://open.garden-fi.com/workflow";
+  var msg = "Open an account at MyFI and we'll both earn $50! Click here: https://open.garden-fi.com/workflow";
   if (navigator.share) {
     navigator.share({ text: msg });
   } else {
